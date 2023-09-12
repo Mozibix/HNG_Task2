5df20ec3df0cb38210663d3e361bed0d06947926
Mozibix <opuzmoses@gmail.com>
2023-09-13 00:43:20 +0100

    chores: updated movieList component

--- (components)/MovieList.jsx
+++ (components)/MovieList.jsx
@@ -2,7 +2,10 @@
 
 import { useRouter } from "next/router";
 import React, { useState, useEffect } from "react";
-function MovieList() {
+import { AiOutlineSearch } from "react-icons/ai";
+
+const MovieList = () => {
+  const [searchQuery, setSearchQuery] = useState("");
   const [movies, setMovies] = useState([]);
   const apiKey = "f2d990ed85300af3d5a6c51e3f08ecf4";
 
@@ -26,10 +29,31 @@ function MovieList() {
     fetchTopMovies();
   }, []);
 
+  const handleSearchInputChange = (e) => {
+    setSearchQuery(e.target.value); // Update the searchQuery state
+  };
+
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <>
+      <div className="movie_search">
+        <div className="movie_search_inner">
+          <span>
+            <AiOutlineSearch />
+          </span>
+          <input
+            type="text"
+            placeholder="What do you want to watch?"
+            value={searchQuery}
+            onChange={handleSearchInputChange} // Handle input change
+          />
+        </div>
+      </div>
       <div className="movie_sec_inner">
-        {movies.map((movie) => (
+        {filteredMovies.map((movie) => (
           <div className="movie_card_container" key={movie.id}>
             <MovieCard key={movie.id} movie={movie} />
           </div>
@@ -37,7 +61,7 @@ function MovieList() {
       </div>
     </>
   );
-}
+};
 
 export const MovieCard = ({ movie }) => {
   const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
