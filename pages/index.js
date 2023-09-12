d8ecd86ce5bf9c87fc32d8584eeead0fb67dc487
Mozibix <opuzmoses@gmail.com>
2023-09-12 12:02:38 +0100

    chores: updated index.js component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Header from "@/(components)/Header";
 import Footer from "@/(components)/Footer";
-import HomePage from "./Home";
+import HomePage from "./HomePage";
 
 export default function Home() {
   return (
