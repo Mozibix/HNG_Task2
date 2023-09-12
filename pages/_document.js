fd79657562ecc658fe5c341e535e5524f5e9f6f7
Mozibix <opuzmoses@gmail.com>
2023-09-12 07:03:03 +0100

    chores: updating the head-element section

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="shortcut icon"
+          href="./svgs/tv_logo_icon.svg"
+          type="image/x-icon"
+        />
+        <title>MovieBox</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
