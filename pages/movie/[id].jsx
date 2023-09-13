a15ac7dcbc0219ccd0bd57650ce72755b9e4c7f9
Mozibix <opuzmoses@gmail.com>
2023-09-13 12:56:06 +0100

    chores: updated movie/[id]  component

--- pages/movie/[id].jsx
+++ pages/movie/[id].jsx
@@ -8,6 +8,15 @@ function MovieDetailsPage() {
   const [movieDetails, setMovieDetails] = useState(null);
   const [error, setError] = useState(null);
   const apiKey = "f2d990ed85300af3d5a6c51e3f08ecf4";
+  const backDropUrl = `https://image.tmdb.org/t/p/w500${movieDetails?.backdrop_path}`;
+
+  const convertToUTC = (dateString) => {
+    const localDate = new Date(dateString);
+    const utcDate = new Date(
+      localDate.getTime() + localDate.getTimezoneOffset() * 60000
+    );
+    return utcDate.toISOString();
+  };
 
   useEffect(() => {
     if (id) {
@@ -18,6 +27,7 @@ function MovieDetailsPage() {
           );
           if (response.ok) {
             const data = await response.json();
+            data.release_date = convertToUTC(data.release_date);
             setMovieDetails(data);
           } else {
             throw new Error("Failed to fetch movie details");
@@ -41,14 +51,38 @@ function MovieDetailsPage() {
 
         <div className="movie_id_details">
           {error ? (
-            <p>Error: {error}</p>
+            <p className="text-center">Error: {error}</p>
           ) : movieDetails ? (
             <>
-              <div>
-                <h1>{movieDetails.title}</h1>
-                <p>Release Date: {movieDetails.release_date}</p>
-                <p>Runtime: {movieDetails.runtime} minutes</p>
-                <p>Overview: {movieDetails.overview}</p>
+              <div className="movie_id_details_inner">
+                <div className="movie_id_details_main">
+                  <div className="movie_id_details_image">
+                    <img src={backDropUrl} alt={movieDetails?.title} />
+                  </div>
+
+                  <div className="movie_id_details_text_sec">
+                    <ul className="movie_id_details_nav">
+                      <li className="font-semibold">{movieDetails?.title}</li>
+                      <span className="diamond_dot"></span>
+                      <li className="font-normal">
+                        {movieDetails?.release_date}
+                      </li>
+                      <span className="diamond_dot"></span>
+                      <li className="font-bold">
+                        {movieDetails?.runtime}{" "}
+                        <span className="font-normal text-xl">minutes</span>
+                      </li>
+                    </ul>
+
+                    <div className="movie_id_details_overview">
+                      <div className="movie_id_details_overview_inner">
+                        <h3>overview:</h3>
+
+                        <p>{movieDetails?.overview}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </>
           ) : (
