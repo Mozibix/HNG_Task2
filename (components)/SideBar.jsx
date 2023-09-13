f028622930e00b95ded4c987ea307880f1f55c10
Mozibix <opuzmoses@gmail.com>
2023-09-13 11:03:27 +0100

    chores: updated sideBar  component

---
+++ (components)/SideBar.jsx
@@ -0,0 +1,58 @@
+import Link from "next/link";
+import React from "react";
+
+const SideBar = () => {
+  return (
+    <>
+      <div className="sidebar">
+        <div className="sidebar_inner">
+          <Link className="cursor-pointer" href={"/"}>
+            <div className="sidebar_logo">
+              <img src="/svgs/tv_logo_icon.svg" alt="logo" />
+              <span>MovieBox</span>
+            </div>
+          </Link>
+
+          <div className="sidebar_menu">
+            <ul>
+              <li>
+                <img src="/svgs/home_icon.svg" alt="sidebar_icon" />
+                <span>Home</span>
+              </li>
+              <li className="active">
+                <img src="/svgs/movie_projector_icon.svg" alt="sidebar_icon" />
+                <span>Movies</span>
+              </li>
+
+              <li>
+                <img src="/svgs/tvshow_icon.svg" alt="sidebar_icon" />
+                <span>Tv Series</span>
+              </li>
+              <li>
+                <img src="/svgs/calender_icon.svg" alt="sidebar_icon" />
+                <span>Upcoming</span>
+              </li>
+            </ul>
+          </div>
+
+          <div className="sidebar_bottom">
+            <div className="sidebar_bottom_quotes">
+              <p>Play movie quizes and earn free tickets</p>
+              <p>50k people are playing now</p>
+
+              <button>Start playing</button>
+            </div>
+            <ul>
+              <li>
+                <img src="/svgs/logout_icon.svg" alt="" />
+                <span>Log out</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SideBar;
